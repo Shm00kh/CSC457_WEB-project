eaf86e113ce6c6ef9110c3fcbd6c737ee51e7ac8
DemoTuna <demaalthu@gmail.com>
2026-05-05 15:15:19 +0300

    removing else in check connection

--- database.php
+++ database.php
@@ -11,8 +11,6 @@ $conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
 // check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}else
-{
-    echo "Connected successfully";
 }
+
 
